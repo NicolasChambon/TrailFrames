39edc789ceac910b1f0e4d0b051e13f75a68f14d
NicolasChambon <nicolas.chambon.dev@gmail.com>
2025-11-06 21:29:00 +0100

    install tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
-
-  const toto = 42;
 
   return (
     <>
@@ -18,7 +15,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className="text-blue-500">Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
